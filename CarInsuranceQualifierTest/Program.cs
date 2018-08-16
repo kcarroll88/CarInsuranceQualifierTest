8079bc121dbfd27ebe980531a21313d3e82bb1ab
Keenan Carroll <keenan.carroll.va@gmail.com>
2018-08-16 16:29:01 -0400

    Made a simple car insurance qualifier

--- CarInsuranceQualifierTest/Program.cs
+++ CarInsuranceQualifierTest/Program.cs
@@ -10,6 +10,19 @@ namespace CarInsuranceQualifierTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? Please use true or false.");
+            bool dui = bool.Parse(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            bool qualified = (age > 15) && (dui == false) && (tickets <= 3);
+            Console.WriteLine("Are you qualified? " + qualified);
+
+            Console.Read();
+
+
         }
     }
 }
